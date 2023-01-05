e5bf0bdc6c4aee97bac8766e6932244fe529ffe5
dashaw92 <dashaw92@gmail.com>
2023-01-05 15:18:20 -0500

    Add Bukkit to signatures

--- TestPlugin/src/lib.rs
+++ TestPlugin/src/lib.rs
@@ -1,14 +1,19 @@
+#![allow(improper_ctypes_definitions)]
+
+use rustbukkit::bukkit::Bukkit;
+
 #[no_mangle]
-pub extern "C" fn onload() {
+pub extern "C" fn onload(_: &dyn Bukkit) {
     println!("Test plugin loaded!");
 }
 
 #[no_mangle]
-pub extern "C" fn onenable() {
+pub extern "C" fn onenable(bukkit: &dyn Bukkit) {
     println!("Test plugin enabled!");
+    bukkit.broadcast_message("hello from TestPlugin!");
 }
 
 #[no_mangle]
-pub extern "C" fn ondisable() {
+pub extern "C" fn ondisable(_: &dyn Bukkit) {
     println!("Test plugin disabled!");
 }
