ba0979a172deeb8560d827dbebc893433ef4cf24
dashaw92 <dashaw92@gmail.com>
2024-03-04 12:46:55 -0500

    Per-plugin arena

--- RustLoader/src/main/java/me/danny/nativeplug/ForeignLoad.java
+++ RustLoader/src/main/java/me/danny/nativeplug/ForeignLoad.java
@@ -1,5 +1,7 @@
 package me.danny.nativeplug;
 
+import org.bukkit.Bukkit;
+
 import java.io.File;
 import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
@@ -7,12 +9,13 @@ import java.lang.foreign.Linker;
 import java.lang.foreign.SymbolLookup;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public final class ForeignLoad {
 
-    private static final Arena PLUGIN_ARENA;
+    private static final Map<String, NativePlugin> PLUGINS = new HashMap<>();
 
     private static final FunctionDescriptor ON_LOAD_DESC = FunctionDescriptor.ofVoid();
     private static final FunctionDescriptor ON_ENABLE_DESC = FunctionDescriptor.ofVoid();
@@ -22,23 +25,28 @@ public final class ForeignLoad {
     private static final MethodHandle DUMMY_ON_ENABLE = MethodHandles.empty(ON_ENABLE_DESC.toMethodType());
     private static final MethodHandle DUMMY_ON_DISABLE = MethodHandles.empty(ON_DISABLE_DESC.toMethodType());
 
-    static {
-        PLUGIN_ARENA = Arena.ofShared();
-    }
-
     static void close() {
-        PLUGIN_ARENA.close();
+        PLUGINS.values().forEach(plug -> {
+            var bukkitPlug = Bukkit.getPluginManager().getPlugin(plug.path());
+            if(bukkitPlug != null) {
+                Bukkit.getPluginManager().disablePlugin(bukkitPlug);
+            }
+
+            plug.arena().close();
+        });
+        PLUGINS.clear();
     }
 
     static NativePlugin loadPlugin(File file) {
         System.out.println(STR."Loading plugin from \{file.getAbsolutePath()}");
         Linker linker = Linker.nativeLinker();
-        SymbolLookup lib = SymbolLookup.libraryLookup(file.toPath(), PLUGIN_ARENA);
+        var arena = Arena.ofConfined();
+        SymbolLookup lib = SymbolLookup.libraryLookup(file.toPath(), arena);
 
         var onLoad = get(linker, lib, "on_load", ON_LOAD_DESC).orElse(DUMMY_ON_LOAD);
         var onEnable = get(linker, lib, "on_enable", ON_ENABLE_DESC).orElse(DUMMY_ON_ENABLE);
         var onDisable = get(linker, lib, "on_disable", ON_DISABLE_DESC).orElse(DUMMY_ON_DISABLE);
-        return new NativePlugin(file.getName(), PLUGIN_ARENA, onLoad, onEnable, onDisable);
+        return new NativePlugin(file.getName(), arena, onLoad, onEnable, onDisable);
     }
 
     private static Optional<MethodHandle> get(Linker linker, SymbolLookup lib, String name, FunctionDescriptor desc) {
